eec716ffd4864b1308434efe535b2a3aab97daba
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-07 23:58:43 +0100

    data-products

--- data/index.ts
+++ data/index.ts
@@ -31,4 +31,76 @@ export const products: Product[] = [
     price: 599,
     id: 'e1',
   },
+  {
+    image:
+      'https://cdn.intersport.se/productimages/220x200/153879201000_10.jpg',
+    title: 'Adidas ALPHABOOST V1 BOOST',
+    description:
+      'Ett par tuffa sneakers med löparinspirerad design från adidas. Ovandelen i mesh låter fötterna andas, med snörning som ger bekväm passform. Mellansulan är utrustad med Boost, Adiprene+ och Bounce som ger utmärkt stötdämpning och fjädrande energiåtergivning. Yttersulan är gjord i slittålig Adiwear™ som klarar av daglig användning.',
+    price: 1299,
+    id: 'e2',
+  },
+  {
+    image:
+      'https://cdn.intersport.se/productimages/220x200/154127401000_10.jpg',
+    title: 'New Balance',
+    description:
+      'New Balance CT574 är den osannolika ikonen bland sneakers. Den har en pålitlig konstruktion med innovativa tekniker och framtagen med premiummaterial. Ovandelen är gjord i en stilsäker kombination av mocka och ventilerande mesh med den klassiska loggan längs sidan. Den sömlösa mellansulan ger bekväm dämpning och hög komfort hela dagen. Tålig yttersula i gummi med bra markgrepp.',
+    price: 999,
+    id: 'e3',
+  },
+  {
+    image:
+      'https://cdn.intersport.se/productimages/220x200/154533801000_10.jpg',
+    title: 'VANS Filmore Decon',
+    description:
+      'Ett par snygga sneakers från Vans som passar perfekt till vardags. Skorna har ovandel i canvas och yttersula i gummi. En tidlös design som funkar till alla typer av vardagsoutfits!',
+    price: 699,
+    id: 'e4',
+  },
+  {
+    image:
+      'https://cdn.intersport.se/productimages/220x200/154756402000_10.jpg',
+    title: 'HOKA Bondi L GTX',
+    description:
+      'Bondi L GTX från Hoka One One är den nya medlemmen i den populära Bondi-familjen. Skorna har en tidlös design med en ovandel i vattentätt läder och ett GORE-TEX-membran som stänger ute väta och håller fötterna torra. Mellansula i EVA ger en lätt dämpning för ett mjukare steg. Yttersulan i slitstarkt gummi som tål blöta underlag.',
+    price: 2599,
+    id: 'e5',
+  },
+  {
+    image:
+      'https://cdn.intersport.se/productimages/220x200/140319501000_10.jpg',
+    title: 'McKinley Tirano NB',
+    description:
+      'Klassisk känga i nubuck. Varmfodrad. Vadderad krage för bekväm passform. Gummisula.',
+    price: 699,
+    id: 'e6',
+  },
+  {
+    image:
+      'https://cdn.intersport.se/productimages/220x200/150856801000_10.jpg',
+    title: 'TIMBERLAND Sprint Trekker',
+    description:
+      'Kängor från Timberland som passar utmärkt för långvandrare som vill ha en bekväm och funktionell sko. Kängorna är tillverkade med en ovandel i ReBOTL™-material som innehåller 50 % återvunnen plast och premium nubuckläder. För en ökad komfort är kängorna utrustade med OrthoLite®-fotbädd som ger dig maximal dämpning när du är ute på dina friluftsaktiviteter.',
+    price: 1299,
+    id: 'e7',
+  },
+  {
+    image:
+      'https://cdn.intersport.se/productimages/220x200/154527301000_10.jpg',
+    title: 'NIKE Air Max Impact 4',
+    description:
+      'Air Max Impact 4 är ett par lätta och smidiga basketskor från Nike med gummi som omsluter sidorna för extra slitstyrka och stabilitet. Max Air-dämpningen i hälen ger utmärkt stötdämpning i landningen. Yttersulan i gummi med fiskbensmönster ger utmärkt grepp vid snabba rörelser upp och ned för basketplan. Diamantformade utskärningar exponerar skummaterialet och ger en mindre vikt.',
+    price: 1149,
+    id: 'e8',
+  },
+  {
+    image:
+      'https://cdn.intersport.se/productimages/220x200/154875501000_10.jpg',
+    title: 'SALMING Rebel M',
+    description:
+      'Rebel är en stabil padelsko för kvinnor från Salming med en låg vikt så att du kan röra dig snabbt över banan och följa spelets snabba vändningar. RebelSKINN är ett tunt syntetlager som täcker hela ovandelen och håller foten på plats och ger ökad stabilitet. Mellansulan med D30-material ger stötdämpning i hälisättningen och ökad komfort. Den slitstarka yttersulan har ett fiskbensmönster som ger utmärkt grepp på padelbanan så att du kan fokusera på ditt spel utan att riskera att tappa fotfästet. Fungerar även till tennis.',
+    price: 999,
+    id: 'e9',
+  }
 ];
