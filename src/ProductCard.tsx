952b206f97f4feeb8b072ce2c95cb95c1d7f5c41
ParhamInBinart <parham.berenjian@gmail.com>
2023-03-08 11:34:50 +0100

    tweak

--- src/ProductCard.tsx
+++ src/ProductCard.tsx
@@ -1,18 +1,14 @@
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card } from "react-bootstrap";
 
-export function ProductCard( {product}:any ) {
+export function ProductCard({ product }: any) {
   return (
-        
-        <Card style={{ width: '16rem' }}>
-        <Card.Img variant="top" src={product.image} />
+    <Card style={{ width: "16rem" }}>
+      <Card.Img variant="top" src={product.image} />
       <Card.Body>
         <Card.Title>{product.title}</Card.Title>
-        <Card.Text>
-          {product.description}
-        </Card.Text>
+        <Card.Text>{product.description}</Card.Text>
         <Button variant="primary">Add to cart</Button>
       </Card.Body>
     </Card>
-    
   );
-}
+};
