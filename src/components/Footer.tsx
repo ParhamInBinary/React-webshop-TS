1a875efbc311c9252cd0a8726402af019705ca37
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-08 15:28:37 +0100

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,20 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-
-
 export function Footer() {
   return (
     <footer>
-      <div style={{backgroundColor: 'rgba(13, 1, 1, 0.5)', color: 'white', padding: '1.5rem', display: 'flex', justifyContent: 'center'}}>
-      Copyrights &#169; reserved by PALIMEDA webshop 2023
+      <div
+        style={{
+          boxShadow: "0 0.5rem 1rem rgba(0, 0, 0, 0.15)",
+          backgroundColor: "rgba(13, 1, 1, 0.5)",
+          color: "white",
+          padding: "1.5rem",
+          display: "flex",
+          justifyContent: "center",
+          bottom: 0,
+          right: 0,
+          left: 0
+        }}
+      >
+        Copyrights &#169; reserved by PALIMEDA webshop 2023
       </div>
     </footer>
   );
