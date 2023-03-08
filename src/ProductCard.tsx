19ce6c8521fccfe95a2f74c0543b86091c6caa63
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-08 12:40:21 +0100

    add bigger cards

--- src/ProductCard.tsx
+++ src/ProductCard.tsx
@@ -1,10 +1,12 @@
+import { Center } from "@chakra-ui/react";
 import { Button, Card } from "react-bootstrap";
+import './index.css';
 
 export function ProductCard({ product }: any) {
   return (
-    <Card style={{ width: "16rem" }}>
-      <Card.Img variant="top" src={product.image} />
-      <Card.Body>
+    <Card className="card" style={{ width: "22rem" }}>
+      <Card.Img variant="top" src={product.image} style={{ width: "18rem", marginLeft: '2rem'}} />
+      <Card.Body className="card-body">
         <Card.Title>{product.title}</Card.Title>
         <Card.Text>{product.description}</Card.Text>
         <Button variant="primary">Add to cart</Button>
