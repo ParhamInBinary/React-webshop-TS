8b323ea1dd97ee0bc72b89ff2e2d72ebdddca82c
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-08 14:18:24 +0100

    fix & styling footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
 
 
 export function Footer() {
   return (
     <footer>
-      <NavbarBs className="footer shadow-lg mb-4">
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link to="/" as={NavLink}>
-              Home
-            </Nav.Link>
-            <Nav.Link to="/Contact" as={NavLink}>
-              Contact
-            </Nav.Link>
-          </Nav>   
-        </Container>
-      </NavbarBs>
+      <div style={{backgroundColor: 'rgba(13, 1, 1, 0.5)', color: 'white', padding: '1.5rem', display: 'flex', justifyContent: 'center'}}>
+      Copyrights &#169; reserved by PALIMEDA webshop 2023
+      </div>
     </footer>
   );
 }
