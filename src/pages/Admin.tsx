bb1b1aa55df7c5e140b70eb516971e6f2053f19d
ParhamInBinart <parham.berenjian@gmail.com>
2023-03-08 16:04:03 +0100

    tweaked style

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -9,7 +9,7 @@ export function Admin() {
     <Container>
       <ListHeader>
         <Row>
-          <Col xs={4}>Title</Col>
+          <Col xs={3}>Title</Col>
           <Col xs={4}>Description</Col>
           <Col>Price</Col>
           <Col>Id</Col>
@@ -18,7 +18,7 @@ export function Admin() {
       <Row>
         {products.map((product) => (
           <ProductItem key={product.id}>
-            <Col xs={4}>
+            <Col xs={3}>
               <img src={product.image} />
               {product.title}
             </Col>
@@ -43,18 +43,21 @@ export function Admin() {
 
 const ListHeader = styled.div `
   font-weight: bold;
+  padding: 1rem;
+  border: 1px solid orange;
 `
 
 const ProductItem = styled.div`
   display: flex;
   border-bottom: 1px solid orange;
   font-size: 14px;
-  padding: 1rem 0;
+  padding: 1rem;
 
   & img {
     width: 3rem;
     margin: 1rem;
   }
+
 `;
 
 const DescContainer = styled.div`
