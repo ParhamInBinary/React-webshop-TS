c9bc45f3cd228549f416c859b5a2afb7d1019611
ParhamInBinart <parham.berenjian@gmail.com>
2023-03-09 15:25:39 +0100

    Logic for adding new product WIP

--- src/components/NewProductForm.tsx
+++ src/components/NewProductForm.tsx
@@ -1,22 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, FloatingLabel } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
+import { generateId, Product } from "../data";
 
 export function NewProductForm() {
   const [validated, setValidated] = useState(false);
+  const [products, setProducts] = useState<Product[]>([]);
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+  useEffect(() => {
+    const storedProducts = JSON.parse(localStorage.getItem("products"));
+    if (storedProducts) {
+      setProducts(storedProducts);
     }
+  }, []);
 
+  const handleSubmit = (event: any) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+        console.log("asd")
+      const imageUrl = form.elements["floatingInput1"].value;
+      const title = form.elements["floatingInput2"].value;
+      const description = form.elements["floatingTextarea2"].value;
+      const price = form.elements["floatingInput3"].value;
+      const id = generateId();
+
+      const newProduct = { id, image: imageUrl, title, description, price };
+      setProducts(prevProducts => [...prevProducts, newProduct]);
+      localStorage.setItem("products", JSON.stringify(products));
+    }
     setValidated(true);
   };
+
   return (
     <>
-      <Form>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <FloatingLabel
           controlId="floatingInput"
           label="Image URL"
