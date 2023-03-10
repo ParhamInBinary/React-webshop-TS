da4317d0d9e9b3de2532b3d630a913cfebfec73a
ParhamInBinart <parham.berenjian@gmail.com>
2023-03-10 11:53:03 +0100

    new comp for editing logic

---
+++ src/components/EditForm.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+interface EditFormProps {
+  item: {
+    title: string;
+    description: string;
+    price: number;
+  };
+  onSave: (updatedItem: {
+    title: string;
+    description: string;
+    price: number;
+  }) => void;
+  onCancel: () => void;
+}
+
+export function EditForm({ item, onSave, onCancel }: EditFormProps) {
+  const [title, setTitle] = useState(item.title);
+  const [description, setDescription] = useState(item.description);
+  const [price, setPrice] = useState(item.price);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSave({
+      ...item,
+      title,
+      description,
+      price,
+    });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="title">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </Form.Group>
+      <Form.Group controlId="description">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </Form.Group>
+      <Form.Group controlId="price">
+        <Form.Label>Price</Form.Label>
+        <Form.Control
+          type="number"
+          value={price}
+          onChange={(event) => setPrice(+event.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Save
+      </Button>
+      <Button variant="secondary" onClick={onCancel}>
+        Cancel
+      </Button>
+    </Form>
+  );
+}
