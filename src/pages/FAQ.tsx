acf4676568758400a73afd801e0f1a009b7cae07
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-12 00:05:54 +0100

    add same bg color as the body

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -2,7 +2,7 @@ import Faq from 'react-faq-component';
 import { Data } from '../data/dataContent'
 
 export function FAQ() {
-  const styles = { rowContentPaddingLeft: '20px' };
+  const styles = { rowContentPaddingLeft: '20px', bgColor: '#f8f9fa' };
 
   return (
     <div>
