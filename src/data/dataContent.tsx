911ff95110a77c698290a288f5be4750ea3c1732
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-11 12:23:26 +0100

    added information for FAQ content in new file

---
+++ src/data/dataContent.tsx
@@ -0,0 +1,22 @@
+interface Data {
+    title: string;
+    rows: {
+      title: string;
+      content: string;
+    }[];
+  }
+
+export const Data = {
+    title: "Frequently Asked Questions (FAQ)",
+    rows: [
+      {
+        title: "How many days until my products arrives?",
+        content: "dsfgdgdf "
+      },
+      {
+        title: "dfdssfdfgdf",
+        content: "hello"
+      },
+    ]
+      
+  };
