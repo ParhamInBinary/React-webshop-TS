bc6e3b14dfd1009f73692b0657827f0ffc329092
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-12 00:02:02 +0100

    add stylingen

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -2,9 +2,11 @@ import Faq from 'react-faq-component';
 import { Data } from '../data/dataContent'
 
 export function FAQ() {
+  const styles = { rowContentPaddingLeft: '20px' };
+
   return (
     <div>
-      <Faq data={Data} />
+        <Faq data={Data} styles={styles} />
     </div>
   );
 }
