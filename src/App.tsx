c254f5c38ca9518492d0d6b9a604c3c8c35d2034
Knightrider85 <daniel@grevensgastro.se>
2023-03-12 18:31:42 +0100

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import { Footer } from "./components/footer/Footer";
 import { Navbar } from "./components/Navbar";
 import { Admin } from "./pages/Admin";
-import { Contact } from "./pages/Contact";
 import { Home } from "./pages/Home";
 import { Products } from "./pages/Products";
 
@@ -16,7 +15,6 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path="/admin" element={<Admin />} />
-            <Route path="/contact" element={<Contact />} />
           </Routes>
         </Container>
         <Footer />
