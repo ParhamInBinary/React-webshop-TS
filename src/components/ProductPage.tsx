e2d998ef745fffc52acfe4080dc63803a21af643
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-13 10:05:48 +0100

    prettier

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,42 +1,45 @@
-import { useLocation } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
-import styled from 'styled-components';
-import { useState } from 'react';
+import { useLocation } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import styled from "styled-components";
+import { useState } from "react";
 
 export function ProductPage() {
-    const location = useLocation();
-    const { product } = location.state;
-    const sizes = ['37', '38', '39', '40', '41', '42', '43', '44', '45', '46'];
-    const [selectedSize, setSelectedSize] = useState(sizes[0]);
+  const location = useLocation();
+  const { product } = location.state;
+  const sizes = ["37", "38", "39", "40", "41", "42", "43", "44", "45", "46"];
+  const [selectedSize, setSelectedSize] = useState(sizes[0]);
 
-
-    return (
-        <ContentDiv>
-        <Card>
-            <Container>
-                <Image src={product.image} alt={product.title} />
-                <ContentDetails>
-                    <Title>{ product.title }</Title>
-                    <Description>{ product.description }</Description>
-                    <Styledp>Price: { product.price } SEK</Styledp>
-                    <div>
-                      <SizeLabel htmlFor="size">Size:</SizeLabel>
-                      <SizeSelect id="size" value={selectedSize} onChange={(event) => setSelectedSize(event.target.value)}>
-                        {sizes.map((size) => (
-                          <option key={size}>{size}</option>
-                        ))}
-                      </SizeSelect>
-                    </div>
-                </ContentDetails>
-            </Container>
-        </Card>
-        </ContentDiv>
-    );
+  return (
+    <ContentDiv>
+      <Card>
+        <Container>
+          <Image src={product.image} alt={product.title} />
+          <ContentDetails>
+            <Title>{product.title}</Title>
+            <Description>{product.description}</Description>
+            <Styledp>Price: {product.price} SEK</Styledp>
+            <div>
+              <SizeLabel htmlFor="size">Size:</SizeLabel>
+              <SizeSelect
+                id="size"
+                value={selectedSize}
+                onChange={(event) => setSelectedSize(event.target.value)}
+              >
+                {sizes.map((size) => (
+                  <option key={size}>{size}</option>
+                ))}
+              </SizeSelect>
+            </div>
+          </ContentDetails>
+        </Container>
+      </Card>
+    </ContentDiv>
+  );
 }
 
-const Styledp = styled.p `
-    font-weight: bold;
-`
+const Styledp = styled.p`
+  font-weight: bold;
+`;
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -46,28 +49,28 @@ const Container = styled.div`
   margin-bottom: 1rem;
 `;
 
-const Image = styled.img `
-    width: 100%;
-    max-width: 490px;
-    object-fit: cover;
+const Image = styled.img`
+  width: 100%;
+  max-width: 490px;
+  object-fit: cover;
 `;
 
-const ContentDetails = styled.div `
-    flex: 1;
-    margin-left: 10px;
+const ContentDetails = styled.div`
+  flex: 1;
+  margin-left: 10px;
 `;
 
-const Title = styled.h1 `
-    font-size: 20px;
+const Title = styled.h1`
+  font-size: 20px;
 `;
 
-const ContentDiv = styled.div `
-    margin-bottom: 6rem;
-    margin-top: 6rem;
+const ContentDiv = styled.div`
+  margin-bottom: 6rem;
+  margin-top: 6rem;
 `;
 
-const Description = styled.p `
-    font-size: 12px;
+const Description = styled.p`
+  font-size: 12px;
 `;
 
 const SizeSelect = styled.select`
