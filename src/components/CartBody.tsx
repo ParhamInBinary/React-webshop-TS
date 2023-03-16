1a24c577c83f7f3ac2d27e50edd3e5f2555f37b6
ParhamInBinart <parham.berenjian@gmail.com>
2023-03-16 09:56:41 +0100

    styling for card body

--- src/components/CartBody.tsx
+++ src/components/CartBody.tsx
@@ -1,12 +1,15 @@
 import { Button, Offcanvas } from "react-bootstrap";
+import { Product } from "../../data";
 
 interface CartBodyProps {
+  cartItems: [];
   show: boolean;
   onHide: () => void;
   clear: () => void;
+  totalCost: number
 }
 
-export function CartBody({ show, onHide, clear }: CartBodyProps) {
+export function CartBody({ cartItems, show, onHide, clear, totalCost }: CartBodyProps) {
   return (
     <>
       <Offcanvas show={show} onHide={onHide} placement="end">
@@ -26,6 +29,49 @@ export function CartBody({ show, onHide, clear }: CartBodyProps) {
               Clear
             </Button>
           </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "1rem",
+            }}
+          >
+            {cartItems.length > 0 ? (
+              cartItems.map((product: Product) => (
+                <div
+                  key={product.id + product.size}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    margin: "1rem",
+                    width: "300px",
+                    borderBottom: "1px solid black",
+                  }}
+                >
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    style={{
+                      width: "120px",
+                      height: "100px",
+                      objectFit: "cover",
+                      marginRight: "1rem",
+                    }}
+                  />
+                  <div>
+                    <div>{product.title}</div>
+                    <div>Size: {product.size}</div>
+                    <div style={{ marginTop: '1rem'}}>{product.price} kr</div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div>Your cart is empty</div>
+            )}
+          </div>
+          <div data-cy="total-price">Total cost: {totalCost} kr</div>
           <Button variant="primary" style={{ marginTop: "2rem" }}>
             Checkout
           </Button>
