3fe02202d28d7c1e0ed10a9cbf2220256af20837
ParhamInBinart <parham.berenjian@gmail.com>
2023-03-22 13:12:21 +0100

    added route for editing item

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 import { Product, products } from "../data";
+import { EditForm } from "./components/EditForm";
 import { Footer } from "./components/footer/Footer";
 import { Navbar } from "./components/Navbar";
 import { NewProductForm } from "./components/NewProductForm";
@@ -12,7 +13,6 @@ import { Home } from "./pages/Home";
 import { ProductPage } from "./pages/ProductPage";
 
 export default function App() {
-  const [items, setItems] = useState<Product[]>(products);
 
   return (
     <>
@@ -28,8 +28,9 @@ export default function App() {
         >
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/admin/product?/:productid?/:editItem?" element={<Admin items={items} setItems={setItems}/>} />
-            <Route path="/admin/product/new" element={<NewProductForm items={items} setItems={setItems}/>} />
+            <Route path="/admin/product?/:productid?/:editItem?" element={<Admin/>} />
+            <Route path="/admin/product/new" element={<NewProductForm />} />
+            <Route path="/admin/product/editItem/:productid" element={<EditForm />} />
             <Route path="/FAQ" element={<FAQ />} />
             <Route path="/product/:productid" element={<ProductPage />} />
             <Route path="/cart/cartitem" element={<CartPage/>}/>
