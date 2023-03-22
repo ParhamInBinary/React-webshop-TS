44bcc0995db6abba31fbd2dc5448848bfe3a19c5
Knightrider85 <daniel@grevensgastro.se>
2023-03-22 13:15:29 +0100

    context for cartpage works

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,33 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-import { Product } from "../../data";
-import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useCart } from "../contexts/cartContext";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { Product } from "../../data"; 
+
 export function CartPage() {
+  const navigate = useNavigate();
+  const { cartItems, clearCart, cart } = useCart();
+  const [totalCost, setTotalCost] = useState(0);
+  const [, setCartItems] = useLocalStorage<Product[]>("cart", []);
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const navigate = useNavigate();
-    const [cartItems, setCartItems] = useState<[]>([]);
-    const [totalCost, setTotalCost] = useState(0);
-  
-    useEffect(() => {
-      const items = JSON.parse(localStorage.getItem("cartItems") || "[]");
-      setCartItems(items);
-      setTotalCost(
-        items.reduce(
-          (total: number, product: Product) => total + Number(product.price),
-          0
-        )
-      );
-    }, []);
+  useEffect(() => {
+    setCartItems(cartItems);
+    setTotalCost(
+      cartItems.reduce(
+        (total: number, product: Product) => total + Number(product.price),
+        0
+      )
+    );
+  }, [cartItems, setCartItems]);
 
-    const clearLocalStorage = () => {
-        localStorage.clear();
-      };
+  const handleCheckout = () => {
+    navigate("/checkout");
+  };
 
-    
   return (
     <body
       style={{
@@ -40,56 +38,60 @@ export function CartPage() {
       <div style={{ display: "flex", justifyContent: "flex-end" }}>
         <Button
           style={{ right: "0px", width: "80px" }}
-          onClick={clearLocalStorage}
+          onClick={clearCart}
         >
           Clear
         </Button>
       </div>
       <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: "1rem",
-            }}
-          >
-            {cartItems.length > 0 ? (
-              cartItems.map((product: Product) => (
-                <div
-                  key={product.id}
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    margin: "1rem",
-                    width: "300px",
-                    borderBottom: "1px solid black",
-                  }}
-                >
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    style={{
-                      width: "120px",
-                      height: "100px",
-                      objectFit: "cover",
-                      marginRight: "1rem",
-                    }}
-                  />
-                  <div>
-                    <div>{product.title}</div>
-                    <div>{product.price} kr</div>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <div>Your cart is empty</div>
-            )}
-          </div>
-          <div data-cy="total-price">Total cost: {totalCost} kr</div>
-          <Button variant="primary" style={{ marginTop: "2rem" }}>
-            Checkout
-          </Button>
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "1rem",
+        }}
+      >
+        {cartItems.length > 0 ? (
+          cartItems.map((product: Product) => (
+            <div
+              key={product.id}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                margin: "1rem",
+                width: "300px",
+                borderBottom: "1px solid black",
+              }}
+            >
+              <img
+                src={product.image}
+                alt={product.title}
+                style={{
+                  width: "120px",
+                  height: "100px",
+                  objectFit: "cover",
+                  marginRight: "1rem",
+                }}
+              />
+              <div>
+                <div>{product.title}</div>
+                <div>{product.price} kr</div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>Your cart is empty</div>
+        )}
+      </div>
+      <div data-cy="total-price">Total cost: {totalCost} kr</div>
+      <Button
+        variant="primary"
+        style={{ marginTop: "2rem" }}
+        onClick={handleCheckout}
+      >
+        Checkout
+      </Button>
     </body>
   );
 }
