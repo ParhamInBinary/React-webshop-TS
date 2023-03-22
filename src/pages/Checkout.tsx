eebc59f3466fd19a91299e50c0adf622612667c2
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-22 13:48:41 +0100

    styling with styled components

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -4,40 +4,33 @@ import { Product } from "../../data";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { OrderForm } from "../components/OrderForm";
+import styled from "styled-components";
+
 export function CartPage() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const navigate = useNavigate();
+  const [cartItems, setCartItems] = useState<[]>([]);
+  const [totalCost, setTotalCost] = useState(0);
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const navigate = useNavigate();
-    const [cartItems, setCartItems] = useState<[]>([]);
-    const [totalCost, setTotalCost] = useState(0);
-  
-    useEffect(() => {
-      const items = JSON.parse(localStorage.getItem("cartItems") || "[]");
-      setCartItems(items);
-      setTotalCost(
-        items.reduce(
-          (total: number, product: Product) => total + Number(product.price),
-          0
-        )
-      );
-    }, []);
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("cartItems") || "[]");
+    setCartItems(items);
+    setTotalCost(
+      items.reduce(
+        (total: number, product: Product) => total + Number(product.price),
+        0
+      )
+    );
+  }, []);
 
-    const clearLocalStorage = () => {
-        localStorage.clear();
-      };
+  const clearLocalStorage = () => {
+    localStorage.clear();
+  };
 
-    
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: "2rem",
-        backgroundColor: "#f8f9fa",
-      }}
-    >
+    <CartContainer>
       <div style={{ display: "flex", justifyContent: "flex-end" }}>
         <Button
           style={{ right: "0px", width: "80px" }}
@@ -46,49 +39,72 @@ export function CartPage() {
           Clear
         </Button>
       </div>
-      <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "start",
-              marginTop: "1rem",
-            }}
-          >
-            {cartItems.length > 0 ? (
-              cartItems.map((product: Product) => (
-                <div
-                  key={product.id}
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    margin: "1rem",
-                    width: "300px",
-                    borderBottom: "1px solid black",
-                  }}
-                >
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    style={{
-                      width: "120px",
-                      height: "100px",
-                      objectFit: "cover",
-                      marginRight: "1rem",
-                    }}
-                  />
-                  <div>
-                    <div>{product.title}</div>
-                    <div>{product.price} kr</div>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <div>Your cart is empty</div>
-            )}
-          </div>
-          <div style={{marginBottom: '4rem'}} data-cy="total-price">Total cost: {totalCost} kr</div>
-          <OrderForm />
-    </div>
+      <ProductsContainer>
+        {cartItems.length > 0 ? (
+          cartItems.map((product: Product) => (
+            <ProductItem key={product.id}>
+              <ProductImage src={product.image} alt={product.title} />
+              <ProductDetails>
+                <ProductTitle>{product.title}</ProductTitle>
+                <ProductPrice>{product.price} kr</ProductPrice>
+              </ProductDetails>
+            </ProductItem>
+          ))
+        ) : (
+          <div>Your cart is empty</div>
+        )}
+      </ProductsContainer>
+      <TotalPrice data-cy="total-price">Total cost: {totalCost} kr</TotalPrice>
+      <OrderForm />
+    </CartContainer>
   );
 }
+
+const CartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  background-color: #f8f9fa;
+`;
+
+
+const ProductsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 1rem;
+`;
+
+const ProductItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem;
+  width: 100%;
+  border-bottom: 1px solid black;
+`;
+
+const ProductImage = styled.img`
+  width: 120px;
+  height: 100px;
+  object-fit: cover;
+  margin-right: 1rem;
+`;
+
+const ProductDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductTitle = styled.div`
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 1.2rem;
+  margin-top: 0.5rem;
+`;
+
+const TotalPrice = styled.div`
+  margin-bottom: 4rem;
+`;
