cd8559d0bada9db19ad8db2fe262c7fa36781b1f
ParhamInBinart <parham.berenjian@gmail.com>
2023-03-28 16:03:25 +0200

    implemented new contex for orders
    Co-authored-by: Mergim Shala <mergim.shala@medieinstitutet.se>
    Co-authored-by: Vanpalo <Knightrider85@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { EditForm } from "./components/EditForm";
 import { Footer } from "./components/footer/Footer";
 import { Navbar } from "./components/Navbar";
 import { NewProductForm } from "./components/NewProductForm";
-import { ToastCart } from "./components/ToastCart";
 import { Admin } from "./pages/Admin";
 import { CartPage } from "./pages/Checkout";
+import { ConfirmationPage } from "./pages/ConfirmationPage";
 import { FAQ } from "./pages/FAQ";
 import { Home } from "./pages/Home";
 import { ProductPage } from "./pages/ProductPage";
@@ -32,6 +32,7 @@ export default function App() {
             <Route path="/FAQ" element={<FAQ />} />
             <Route path="/product/:productid" element={<ProductPage />} />
             <Route path="/confirmation" element={<CartPage/>}/>
+            <Route path="/confirmationPage" element={<ConfirmationPage />}/>
           </Routes>
         </Container>
       </main>
