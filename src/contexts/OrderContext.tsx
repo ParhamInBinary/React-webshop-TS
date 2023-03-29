a3d48ac8c2d5df22656d533d4e04d9375ed2ce88
ParhamInBinart <parham.berenjian@gmail.com>
2023-03-29 12:02:52 +0200

    set order number and items on confirmation page
    Co-authored-by: Vanpalo <Knightrider85@users.noreply.github.com>

--- src/contexts/OrderContext.tsx
+++ src/contexts/OrderContext.tsx
@@ -3,6 +3,7 @@ import { OrderDetails } from "../components/OrderForm";
 import { useCart } from "./cartContext";
 
 interface OrderContextType {
+  orderNumber: number;
   orderDetails: OrderDetails;
   setOrderDetails: (values: Partial<OrderDetails>) => void;
 }
@@ -14,8 +15,9 @@ export function useOrderContext() {
 }
 
 export function OrderProvider({ children }: PropsWithChildren) {
-  const { cartItems } = useCart();
+  const { cartItems, setCartItems, totalCost } = useCart();
 
+  const [orderNumber, setOrderNumber] = useState(0);
   const [orderDetails, updateOrderDetails] = useState<OrderDetails>({
     name: "",
     address: "",
@@ -25,15 +27,33 @@ export function OrderProvider({ children }: PropsWithChildren) {
     phone: "",
   });
 
+  const getOrderNumber = () => {
+    setOrderNumber(Math.floor(Math.random() * (1000000000 - 10000) + 10));
+  };
+
   const setOrderDetails = (values: Partial<OrderDetails>) => {
+    const item = cartItems.map((cartItem) => (
+      <div style={{ padding: '1rem', }}>
+        <img src={cartItem.image} style={{ width: '5rem'}} />
+        <div>{cartItem.title}</div>
+        <div>{cartItem.price + ' kr'}</div>
+      </div>
+    ));
+
+    getOrderNumber();
     updateOrderDetails((prevOrderDetails) => ({
       ...prevOrderDetails,
       ...values,
+      item,
+      totalCost,
     }));
+    setCartItems([]);
   };
 
   return (
-    <OrderContext.Provider value={{ orderDetails, setOrderDetails }}>
+    <OrderContext.Provider
+      value={{ orderNumber, orderDetails, setOrderDetails }}
+    >
       {children}
     </OrderContext.Provider>
   );
